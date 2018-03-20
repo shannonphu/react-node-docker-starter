0ef93565326d6b1628537dca66464c9e638e4d9f
shannonphu <shannonphu@sbcglobal.net>
2018-03-20 14:48:51 -0700

    cleared out fetch error handling

--- client/src/utils/httpHelper.js
+++ client/src/utils/httpHelper.js
@@ -6,20 +6,28 @@ const headers = new Headers();
 headers.set('Content-Type', 'application/json');
 
 const reqConf = {
-   headers: headers,
-   credentials: 'include',
+    headers: headers,
+    credentials: 'include',
 };
 
 // Helper functions for the comon request types
 export function smartFetch(method, endpoint, body) {
-      return fetch(baseURL + endpoint, {
-         method: method,
-         body: body && JSON.stringify(body),
-         ...reqConf
-      })
-      .then(response => {
-         return response.ok ? response : createErrorPromise(response)
-      })
+    return fetch(baseURL + endpoint, {
+        method: method,
+        body: body && JSON.stringify(body),
+        ...reqConf
+    }).catch((err) => {
+        return Promise.reject(`${method} ${endpoint} with body ${body} failed.`);
+    });
+}
+
+/**
+ * make a get request
+ * @param {string} endpoint
+ * @returns {Promise}
+ */
+export function get(endpoint) {
+    return smartFetch('GET', endpoint);
 }
 
 /**
@@ -29,7 +37,7 @@ export function smartFetch(method, endpoint, body) {
  * @returns {Promise}
  */
 export function post(endpoint, body) {
-   return smartFetch('POST', endpoint, body);
+    return smartFetch('POST', endpoint, body);
 }
 
 /**
@@ -39,94 +47,9 @@ export function post(endpoint, body) {
  * @returns {Promise}
  */
 export function put(endpoint, body) {
-   return smartFetch('PUT', endpoint, body);
-}
-
-/**
- * make a get request
- * @param {string} endpoint
- * @returns {Promise}
- */
-export function get(endpoint) {
-   return smartFetch('GET', endpoint);
+    return smartFetch('PUT', endpoint, body);
 }
 
 export function del(endpoint) {
-   return smartFetch('DELETE', endpoint);
-}
-
-// Functions for performing the api requests
-
-// Handle response with non-200 status by returning a Promise that rejects,
-// with reason: array of one or more error strings suitable for display.
-function createErrorPromise(response) {
-   if (response.status === 400)
-      return Promise.resolve(response)
-         .then(response => response.json())
-         .then(errorList => Promise.reject(errorList.length
-            ? errorList.map(err => errorTranslate(err.tag, navigator.languages
-               ? navigator.languages[0]
-               : (navigator.language || navigator.userLanguage)))
-            : ["Unknown error"]));
-   else
-      return Promise.reject(["Unknown error"]);
-}
-
-const errMap = {
-   en: {
-      missingField: 'Field missing from request: ',
-      badValue: 'Field has bad value: ',
-      notFound: 'Entity not present in DB',
-      badLogin: 'Email/password combination invalid',
-      dupEmail: 'Email duplicates an existing email',
-      noTerms: 'Acceptance of terms is required',
-      forbiddenRole: 'Role specified is not permitted.',
-      noOldPwd: 'Change of password requires an old password',
-      oldPwdMismatch: 'Old password that was provided is incorrect.',
-      dupTitle: 'Conversation title duplicates an existing one',
-      dupEnrollment: 'Duplicate enrollment',
-      forbiddenField: 'Field in body not allowed.',
-      queryFailed: 'Query failed (server problem).'
-   },
-   es: {
-      missingField: '[ES] Field missing from request: ',
-      badValue: '[ES] Field has bad value: ',
-      notFound: '[ES] Entity not present in DB',
-      badLogin: '[ES] Email/password combination invalid',
-      dupEmail: '[ES] Email duplicates an existing email',
-      noTerms: '[ES] Acceptance of terms is required',
-      forbiddenRole: '[ES] Role specified is not permitted.',
-      noOldPwd: '[ES] Change of password requires an old password',
-      oldPwdMismatch: '[ES] Old password that was provided is incorrect.',
-      dupTitle: '[ES] Conversation title duplicates an existing one',
-      dupEnrollment: '[ES] Duplicate enrollment',
-      forbiddenField: '[ES] Field in body not allowed.',
-      queryFailed: '[ES] Query failed (server problem).'
-   },
-   swe: {
-      missingField: 'Ett fält saknas: ',
-      badValue: 'Fält har dåligt värde: ',
-      notFound: 'Entitet saknas i DB',
-      badLogin: 'Email/lösenord kombination ogilltig',
-      dupEmail: 'Email duplicerar en existerande email',
-      noTerms: 'Villkoren måste accepteras',
-      forbiddenRole: 'Angiven roll förjuden',
-      noOldPwd: 'Tidiagre lösenord krav för att updatera lösenordet',
-      oldPwdMismatch: 'Tidigare lösenord felaktigt',
-      dupTitle: 'Konversationstitel duplicerar tidigare existerande titel',
-      dupEnrollment: 'Duplicerad inskrivning',
-      forbiddenField: 'Förbjudet fält i meddelandekroppen',
-      queryFailed: 'Förfrågan misslyckades (server problem).'
-   }
-}
-
-/**
- * TODO perhaps should return a Promise to conform with the
- * rest of the api functions
- *
- * @param {string} errTag
- * @param {string} lang
- */
-export function errorTranslate(errTag, lang = 'en') {
-   return errMap[lang.split('-')[0]][errTag] || 'Unknown Error!';
+    return smartFetch('DELETE', endpoint);
 }
