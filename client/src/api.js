f21163d8c6d07cae8c71bfa8e8c219249967595b
shannonphu <shannonphu@sbcglobal.net>
2018-04-18 21:26:13 -0700

    Migrated http lib to axios

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,12 @@
 import { get } from './utils/httpHelper';
 
+const config = require('./config');
+const serverBaseURL = `http://${config.server.host}:${config.server.port}`;
+
 /**
  * @description Makes server request for username
  * @returns {JSON} username in JSON format
  */
 export function getUsername() {
-   return get("/user/name").then(res => res.json());
+    return get(`${serverBaseURL}/user/name`).then(res => res.json());
 }
